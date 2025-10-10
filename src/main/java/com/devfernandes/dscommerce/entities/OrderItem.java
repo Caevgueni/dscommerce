402c32aeaf41e02ea91a1b27dd920e0be83f6499
Carlos Fernandes <itais1@hotmail.com>
2025-10-10 23:15:15 +0100

    Muitos-para-muitos com classe de associação PARTE 1

---
+++ src/main/java/com/devfernandes/dscommerce/entities/OrderItem.java
@@ -0,0 +1,71 @@
+package com.devfernandes.dscommerce.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="tb_orderItem")
+public class OrderItem {
+	
+	@EmbeddedId
+	OrderItemPK id = new OrderItemPK();
+	
+	private Integer quantity;
+	private Double  price;
+	
+	
+	public OrderItem () {
+		
+	}
+
+
+	public OrderItem(Product product, Order order, Integer quantity, Double price) {
+		id.setProduct(product);
+		id.setOrder(order);
+		this.quantity = quantity;
+		this.price = price;
+	}
+
+
+	public Order getOrder() {
+		
+		return id.getOrder();
+		
+	}
+	
+	public void setOrder(Order order) {
+	  id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void SetProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
+	
+}
--- src/main/java/com/devfernandes/dscommerce/entities/Product.java
+++ src/main/java/com/devfernandes/dscommerce/entities/Product.java
@@ -11,14 +11,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
-
-
 @Entity
-@Table(name="tb_product")
+@Table(name = "tb_product")
 public class Product {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -27,93 +26,70 @@ public class Product {
 	private String description;
 	private Double price;
 	private String imagUrl;
-	
-	
+
 	@ManyToMany
-	@JoinTable(name = "tb_product_category",
-	joinColumns = @JoinColumn(name = "product_id"),
-	inverseJoinColumns = @JoinColumn(name = "category_id"))
-	private Set<Category>  categories = new HashSet<>();
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
+	
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem>  items = new HashSet<>();
 
 	public Product() {
-		
+
 	}
 
-
-
 	public Product(Long id, String name, String description, Double price, String imagUrl) {
-		
+
 		this.id = id;
 		this.name = name;
 		this.description = description;
 		this.price = price;
 		this.imagUrl = imagUrl;
 	}
-
-
 
 	public Long getId() {
 		return id;
 	}
 
-
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
 
 	public String getDescription() {
 		return description;
 	}
 
-
-
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-
 
 	public Double getPrice() {
 		return price;
 	}
 
-
-
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-
-
 
 	public String getImagUrl() {
 		return imagUrl;
 	}
 
-
-
 	public void setImagUrl(String imagUrl) {
 		this.imagUrl = imagUrl;
 	}
 
-
-
 	public Set<Category> getCategories() {
 		return categories;
 	}
-	
+
 }
