17f34685da59fb604e289b8d756dc3ba83859b2f
Carlos Fernandes <itais1@hotmail.com>
2025-10-15 11:34:19 +0100

    Busca paginada de produtos

--- src/main/java/com/devfernandes/dscommerce/entities/services/ProductService.java
+++ src/main/java/com/devfernandes/dscommerce/entities/services/ProductService.java
@@ -3,6 +3,8 @@ package com.devfernandes.dscommerce.entities.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,7 +19,14 @@ public class ProductService {
 	@Autowired
 	private ProductRepository  productRepository;
 	
-
+	
+	@Transactional(readOnly=true)
+     public Page<ProductDTO> findAll(Pageable pageable){
+    	 
+    	 Page<Product> result = productRepository.findAll(pageable);
+    	 return result.map(x -> new ProductDTO(x));
+     }
+	
 	@Transactional(readOnly=true)
 	public ProductDTO findById(Long id){
 		
